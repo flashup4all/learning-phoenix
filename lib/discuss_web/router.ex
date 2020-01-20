d321abb9da1c5533613e572526707430e16a0da0
Bardeson Lucky <mac@macs-MacBook-Pro.local>
2020-01-20 14:07:33 +0100

    learning elixir, phoenix, ecto

---
+++ lib/discuss_web/router.ex
@@ -0,0 +1,33 @@
+defmodule DiscussWeb.Router do
+  use DiscussWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", DiscussWeb do
+    pipe_through :browser
+
+    # get "/", PageController, :index
+    # get "/", TopicController, :index
+    # get "/topic/new", TopicController, :new
+    # post "/topic", TopicController, :create
+    # get "/topic/:id/edit", TopicController, :edit
+    # put "/topic/:id", TopicController, :update
+    # delete "/topic/:id", TopicController, :delete
+    resources "/", TopicController
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", DiscussWeb do
+  #   pipe_through :api
+  # end
+end
